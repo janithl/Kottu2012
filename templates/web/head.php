2ea128080730904bc912ccfc1af93759f17bb25b
Janith <janith@yamu.lk>
2016-10-25 00:29:04 +0530

    bootstrap meta tag weirdness

--- templates/web/head.php
+++ templates/web/head.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<title><?php echo $this->title; ?></title>
-
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $this->title; ?></title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,500&amp;subset=latin-ext,sinhala"> 
 	<link rel="stylesheet" type="text/css" media="all" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" media="all" href="<?php echo config('basepath'); ?>/static/css/style.css">
